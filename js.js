d05686c5b107869f39c2a03ffffa7c5d243fbce1
CED0001 <cedricver@outlook.com>
2021-03-18 12:20:10 +0100

    Julien, Ahmed 18.03.21

--- js.js
+++ js.js
@@ -2,19 +2,18 @@ console.log(`Hello World!`);
 console.log(`Dit is nog een change (testing)`);
 console.log(`GOEIEMORGEN`);
 
-// var myModal = new bootstrap.Modal(document.getElementById("myModal"), options);
-// console.log(myModal);
+// ================ Trajecten learning snacks ====================
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-// var myModal = new bootstrap.Modal(document.getElementById("myModal"), {
-//   keyboard: false
-// });
-// myModal.toggle();
-
-// var myModal = document.getElementById("myModal");
-// var myInput = document.getElementById("myInput");
-
-// myModal.addEventListener("shown.bs.modal", function () {
-//   myInput.focus();
-// });
-
-// myModal.shown();
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
